acd3797dcb761af6b5f03e55e7c69767eaec8965
SheikhSaidee07 <sheikh.saidee@iquasar.com>
2023-07-31 12:09:52 +0530

    some logic changes

---
+++ src/components/BasicTable.js
@@ -0,0 +1,108 @@
+import React, { useMemo, useState } from "react";
+import { useTable, useSortBy } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+import "./table.css";
+import { Checkbox } from "./Checkbox";
+
+export const BasicTable = () => {
+  let columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const [number, setNumber] = React.useState(10);
+  const inputRef = React.useRef();
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    allColumns,
+    getToggleHideAllColumnsProps,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  );
+  const handleChange = () => {
+    setNumber(parseInt(inputRef.current.value));
+  };
+  const firstPageRows = !number ? rows.slice(0, 10) : rows.slice(0, number);
+
+  return (
+    <>
+      <div>
+        <h2>Please Enter the Number of Rows U want to show</h2>
+        <div>
+          <label>Numner of Rows You want to show : </label>
+          <input type="text" ref={inputRef} onChange={handleChange}></input>
+        </div>
+      </div>
+      <div className="checkbox">
+        <div className="checkbox-content">
+          <Checkbox {...getToggleHideAllColumnsProps()} /> Toggle All
+        </div>
+        {allColumns.map((column) => (
+          <div key={column.id} className="checkbox-content">
+            <label>
+              <input type="checkbox" {...column.getToggleHiddenProps()} />{" "}
+              {column.Header}
+            </label>
+          </div>
+        ))}
+        <br />
+      </div>
+      <div>
+        <p>
+          First Click on each Column if you want the table to be Sorted in
+          Ascending Or Desending
+        </p>
+      </div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
+  );
+};
